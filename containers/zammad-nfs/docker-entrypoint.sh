c0c67b28678fc2a31542493410e1c4b6c757ef89
André Bauer <monotek23@gmail.com>
2017-11-22 20:04:01 +0100

    fgfg

--- containers/zammad-nfs/docker-entrypoint.sh
+++ containers/zammad-nfs/docker-entrypoint.sh
@@ -4,11 +4,10 @@ set -e
 
 if [ "$1" = 'zammad-nfs' ]; then
   echo "create & mount tmpfs"
-  test -d ${NFS_DIR}/tmp || mkdir -p ${NFS_DIR}/tmp
+  test -d ${NFS_DIR} || mkdir -p ${NFS_DIR}
   chmod 777 ${NFS_DIR}
-  mount -t tmpfs -o size=${TMPFS_SIZE} none ${NFS_DIR}/tmp
-  chown -R 1000:1000 ${NFS_DIR}
-
+  mount -t tmpfs -o size=${TMPFS_SIZE} none ${NFS_DIR}
+  
   echo "create nfs exports"
   echo "# NFS Export for Zammad" > /etc/exports
   echo "${NFS_DIR} *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
