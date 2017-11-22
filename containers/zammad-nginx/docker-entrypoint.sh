4d848ed999bf223a657628796eaf69b4f6dcf508
André Bauer <monotek23@gmail.com>
2017-11-22 18:50:36 +0100

    nginx fixes

--- containers/zammad-nginx/docker-entrypoint.sh
+++ containers/zammad-nginx/docker-entrypoint.sh
@@ -2,26 +2,13 @@
 
 set -e
 
-function check_nfs_available {
-  until (echo > /dev/tcp/zammad-nfs/2049) &> /dev/null; do
-    echo "waiting for zammads nfsserver to be ready..."
-    sleep 2
-  done
-  echo "nginx can access nfs server now..."
-}
-
-function mount_nfs {
-  if [ -n "$(env|grep KUBERNETES)" ]; then
-    check_nfs_available
-    test -d ${ZAMMAD_DIR} || mkdir -p ${ZAMMAD_DIR}
-    mount -t nfs4 zammad-nfs:/ ${ZAMMAD_DIR}
-  fi
-}
-
 # zammad-railsserver
 if [ "$1" = 'zammad-nginx' ]; then
 
-  mount_nfs
+  if [ -n "$(env|grep KUBERNETES)" ]; then
+    test -d ${ZAMMAD_DIR} || mkdir -p ${ZAMMAD_DIR}
+    mount -t nfs4 zammad-nfs:/ ${ZAMMAD_DIR}
+  fi
 
   /usr/sbin/nginx -g 'daemon off;'
 
