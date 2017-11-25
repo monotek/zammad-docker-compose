3d0955f54efe1f2a7c716a4db628f24d06891bb8
André Bauer <monotek23@gmail.com>
2017-11-25 16:15:47 +0100

    bam

--- containers/zammad-nfs/docker-entrypoint.sh
+++ containers/zammad-nfs/docker-entrypoint.sh
@@ -5,12 +5,15 @@ set -e
 if [ "$1" = 'zammad-nfs' ]; then
   echo "creating nfs dir"
 
-  test -d ${NFS_DIR} || mkdir -p ${NFS_DIR}
-  chmod -R 777 ${NFS_DIR}
+  test -d ${NFS_DIR}/data || mkdir -p ${NFS_DIR}/data
+  test -d ${NFS_DIR}/uploads || mkdir -p ${NFS_DIR}/uploads
+  chmod -R 777  ${NFS_DIR}/data ${NFS_DIR}/uploads
 
   echo "create nfs exports"
   echo "# NFS Export for Zammad" > /etc/exports
   echo "${NFS_DIR} *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
+  echo "${NFS_DIR}/data *(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
+  echo "${NFS_DIR}/uploads *(rw,sync,no_subtree_checkno_root_squash)" >> /etc/exports
 
   exec runsvdir /etc/sv
 fi
