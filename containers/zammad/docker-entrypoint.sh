0b34c937fed5c36d06fafbb1997d7ab5d3190934
André Bauer <monotek23@gmail.com>
2017-11-26 12:47:34 +0100

    dg

--- containers/zammad/docker-entrypoint.sh
+++ containers/zammad/docker-entrypoint.sh
@@ -3,8 +3,16 @@
 set -e
 
 function check_install_update_ready {
-  until [ -f "${INSTALL_UPDATE_READY_FILE}" ]; then
+  until [ -f "${ZAMMAD_READY_FILE}" ]; then
     echo "waiting for install or update to be ready..."
+    sleep 5
+  done
+}
+
+function check_railsserver_available {
+  # wait for zammad process coming up
+  until (echo > /dev/tcp/zammad-railsserver/3000) &> /dev/null; do
+    echo "waiting for zammads railsserver to be ready..."
     sleep 5
   done
 }
@@ -12,7 +20,7 @@ function check_install_update_ready {
 function mount_nfs {
   if [ -n "$(env|grep KUBERNETES)" ]; then
     test -d ${ZAMMAD_DIR} || mkdir -p ${ZAMMAD_DIR}
-    mount -t nfs4 zammad-nfs:/ /opt/zammad
+    mount -t nfs4 zammad-nfs:/data /opt/zammad
   fi
 }
 
@@ -27,7 +35,8 @@ if [ "$1" = 'zammad-init' ]; then
   mount_nfs
 
   # install / update zammad
-  rsync -a --delete --exclude 'storage/fs/*' ${ZAMMAD_TMP_DIR}/ ${ZAMMAD_DIR}
+  rsync -a --delete --exclude 'storage/fs/*' --exclude 'public/assets/images/*' ${ZAMMAD_TMP_DIR}/ ${ZAMMAD_DIR}
+  rsync -a ${ZAMMAD_TMP_DIR}/public/assets/images/ ${ZAMMAD_DIR}/public/assets/images
 
   echo "initialising / updating database..."
   # db mirgrate
@@ -53,21 +62,28 @@ if [ "$1" = 'zammad-init' ]; then
   chown -R ${ZAMMAD_USER}:${ZAMMAD_USER} ${ZAMMAD_DIR}
 
   # create install ready file
-  touch ${INSTALL_UPDATE_READY_FILE}
+  echo "zammad-init" > ${ZAMMAD_READY_FILE}
 fi
 
 
 # zammad nginx
 if [ "$1" = 'zammad-nginx' ]; then
-  check_install_update_ready
+  check_railsserver_available
 
   mount_nfs
-
-  echo "starting nginx..."
 
   if [ -n "$(env|grep KUBERNETES)" ]; then
     sed -i -e 's#server zammad-railsserver:3000#server zammad:3000#g' -e 's#zammad-websocket:6042#zammad:6042#g' /etc/nginx/sites-enabled/default
   fi
+
+  until [ -n "$(grep zammad-railsserver < ${ZAMMAD_READY_FILE})" ] && [ -n "$(grep zammad-scheduler < ${ZAMMAD_READY_FILE})" ] && [ -n "$(grep zammad-websocket < ${ZAMMAD_READY_FILE})" ] ; do
+    echo "nginx waiting for all zammad services to start..."
+    sleep 5
+  done
+
+  rm ${ZAMMAD_READY_FILE}
+
+  echo "starting nginx..."
 
   exec /usr/sbin/nginx -g 'daemon off;'
 fi
@@ -81,6 +97,8 @@ if [ "$1" = 'zammad-railsserver' ]; then
 
   echo "starting railsserver..."
 
+  echo "zammad-railsserver" >> ${ZAMMAD_READY_FILE}
+
   exec gosu ${ZAMMAD_USER}:${ZAMMAD_USER} bundle exec puma -b tcp://0.0.0.0:3000 -e ${RAILS_ENV}
 fi
 
@@ -93,6 +111,8 @@ if [ "$1" = 'zammad-scheduler' ]; then
 
   echo "starting scheduler..."
 
+  echo "zammad-scheduler" >> ${ZAMMAD_READY_FILE}
+
   exec gosu ${ZAMMAD_USER}:${ZAMMAD_USER} bundle exec script/scheduler.rb run
 fi
 
@@ -104,6 +124,8 @@ if [ "$1" = 'zammad-websocket' ]; then
   mount_nfs
 
   echo "starting websocket server..."
+
+  echo "zammad-websocket" >> ${ZAMMAD_READY_FILE}
 
   exec gosu ${ZAMMAD_USER}:${ZAMMAD_USER} bundle exec script/websocket-server.rb -b 0.0.0.0 start
 fi
