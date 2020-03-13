f265313313bd059b83c1f0a73cfb52eef403a89d
André Bauer <monotek23@gmail.com>
2020-03-13 09:04:18 +0100

    test
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/tests.sh
+++ .github/tests.sh
@@ -30,13 +30,21 @@ echo
 
 
 echo 
-echo "Fill db with some random data"
+echo "Fill DB with some random data"
 docker exec zammad-docker-compose_zammad-railsserver_1 rails r "FillDB.load(agents: 1,customers: 1,groups: 1,organizations: 1,overviews: 1,tickets: 1)"
+
+echo
+echo "DB fill successful :)"
+echo
 
 echo
 echo "create user via api"
 echo
-curl --silent --fail --show-error -u info@zammad.org:Zammad -H "Content-Type: application/json" -X POST -d '{"firstname":"Bob","lastname":"Smith","email":"testuser@example.com","roles":["Customer"],"password":"some_password"}' 'http://127.0.0.1/api/v1/users'
+curl --cookie --silent --fail --show-error -u info@zammad.org:Zammad -H "Content-Type: application/json" -X POST -d '{"firstname":"Bob","lastname":"Smith","email":"testuser@example.com","roles":["Customer"],"password":"some_password"}' 'http://127.0.0.1/api/v1/users'
+
+echo
+echo "create user successful :)"
+echo
 
 # echo 
 # echo "search user"
@@ -44,10 +52,17 @@ curl --silent --fail --show-error -u info@zammad.org:Zammad -H "Content-Type: ap
 # curl --silent --fail --show-error -u info@zammad.org:Zammad 'http://localhost/api/v1/users/search?query=Smith&limit=10&expand=true'
 
 # echo
+# echo "search user successful :)"
+# echo
+
+# echo
 # echo "create ticket"
 # echo
 # curl --silent --fail --show-error -u info@zammad.org:Zammad -H "Content-Type: application/json" -X POST -d '{"title":"Help me!","group": "Users","article":{"subject":"some subject","body":"some message","type":"note","internal":false},"customer":"testuser@example.com","note": "some note"}' 'http://localhost/api/v1/tickets'
 
+# echo
+# echo "create ticket successful :)"
+# echo
 
 
 
