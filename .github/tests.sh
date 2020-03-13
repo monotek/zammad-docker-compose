8b194c55a2555e8ead3f4e7301c1c854bf762857
André Bauer <monotek23@gmail.com>
2020-03-13 18:41:47 +0100

    test
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/tests.sh
+++ .github/tests.sh
@@ -20,30 +20,23 @@ echo "Success - Zammad is up :)"
 echo
 echo "Execute autowizard..."
 echo
-
-#curl -I --silent --fail --show-error "http://localhost/#getting_started/auto_wizard/docker_compose_token" > /dev/null
 docker exec zammad-docker-compose_zammad-railsserver_1 rake zammad:setup:auto_wizard
-
 echo 
 echo "Autowizard executed successful :)"
 echo 
-
-
-echo 
 echo "Fill DB with some random data"
 docker exec zammad-docker-compose_zammad-railsserver_1 rails r "FillDB.load(agents: 1,customers: 1,groups: 1,organizations: 1,overviews: 1,tickets: 1)"
-
 echo
 echo "DB fill successful :)"
 echo
 
 
+echo
+echo "create user via api"
+echo
+docker exec zammad-docker-compose_zammad-nginx_1 curl -I localhost
 
-# echo
-# echo "create user via api"
-# echo
-
-# curl -I http://localhost/api/v1/users
+docker exec zammad-docker-compose_zammad-nginx_1 curl --cookie --silent --fail --show-error -u info@zammad.org:Zammad -H "Content-Type: application/json" -X POST -d '{"firstname":"Bob","lastname":"Smith","email":"testuser@example.com","roles":["Customer"],"password":"some_password"}' 'http://localhost/api/v1/users'
 
 # #curl --cookie --silent --fail --show-error -u info@zammad.org:Zammad -H "Content-Type: application/json" -X POST -d '{"firstname":"Bob","lastname":"Smith","email":"testuser@example.com","roles":["Customer"],"password":"some_password"}' 'http://localhost/api/v1/users'
 
@@ -51,14 +44,14 @@ echo
 # echo "create user successful :)"
 # echo
 
-echo 
-echo "search user"
-echo
-curl --silent --fail --show-error -u info@zammad.org:Zammad "http://$(docker inspect zammad-docker-compose_zammad-nginx_1 | grep '"IPAddress": "[0-9]*\.[0-9]*\.[0-9]\.[0-9]*' | sed -e 's#.*: "##g' -e 's#",##g')/api/v1/users/search?query=Smith&limit=10&expand=true"
+# echo 
+# echo "search user"
+# echo
+# curl --silent --fail --show-error -u info@zammad.org:Zammad "http://$(docker inspect zammad-docker-compose_zammad-nginx_1 | grep '"IPAddress": "[0-9]*\.[0-9]*\.[0-9]\.[0-9]*' | sed -e 's#.*: "##g' -e 's#",##g')/api/v1/users/search?query=Smith&limit=10&expand=true"
 
-echo
-echo "search user successful :)"
-echo
+# echo
+# echo "search user successful :)"
+# echo
 
 # echo
 # echo "create ticket"
